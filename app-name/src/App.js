b535eb6f95f46c8ebce568fd2096b83a87797ab4
Aldrich Lapid <alapid@LABA.PSWAS.LOCAL>
2023-05-11 10:08:26 -0700

    files completed

---
+++ app-name/src/App.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// WE IMPORT OUR COMPONENTS
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  //variable with your apiKey
+  const apiKey = "283ddb7a";
+
+  //State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
+  //This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  // USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+      <Title title={movie.Title}/>
+    </div>
+  );
+}
